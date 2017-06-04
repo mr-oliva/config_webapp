475e94780c63259b7857768ba2208d0875e54b66
ryo2851 <kutsuzawa2851@gmail.com>
2017-06-05 05:55:02 +0900

    add sql

---
+++ create_toppage.sql
@@ -0,0 +1,18 @@
+create table services (
+  id INT UNSIGNED AUTO_INCREMENT primary key,
+  name varchar(30),
+  url text,
+  icon_url text
+);
+
+create table layouts (
+  goo_id varchar(30),
+  service_id INT UNSIGNED,
+  foreign key (service_id)
+    references services (id),
+  icon_number INT UNSIGNED NOT NULL,
+  created DATETIME DEFAULT NULL,
+  modified DATETIME DEFAULT NULL,
+  primary key ('goo_id', 'service_id')
+);
+
---
+++ insert_table.sql
@@ -0,0 +1,42 @@
+INSERT INTO services (name,url,icon_url)
+    VALUES ('twitter', 'https://twitter.com/', '/img/service_images/twitter.png');
+
+
+INSERT INTO services (name,url,icon_url)
+    VALUES ('facebook', 'https://facebook.com/', '/img/service_images/facebook.png');
+
+
+INSERT INTO services (name,url,icon_url)
+    VALUES ('instagram', 'https://instagram.com/', '/img/service_images/insta.png');
+
+INSERT INTO services (name,url,icon_url)
+    VALUES ('website', '', '/img/service_images/website.png');
+
+
+INSERT INTO services (name,url,icon_url)
+    VALUES ('email', '', '/img/service_images/email.png');
+
+
+INSERT INTO services (name,url,icon_url)
+    VALUES ('note', '', '/img/service_images/note.png');
+
+
+INSERT INTO layouts (goo_id,service_id,icon_number, created, modified)
+    VALUES ('ryo', 1, 1, NOW(), NOW());
+
+
+INSERT INTO layouts (goo_id,service_id,icon_number, created, modified)
+    VALUES ('ryo', 2, 2, NOW(), NOW());
+
+
+INSERT INTO layouts (goo_id,service_id,icon_number, created, modified)
+    VALUES ('ryo', 3, 3, NOW(), NOW());
+
+
+INSERT INTO layouts (goo_id,service_id,icon_number, created, modified)
+    VALUES ('ryo', 4, 4, NOW(), NOW());
+
+
+INSERT INTO layouts (goo_id,service_id,icon_number, created, modified)
+    VALUES ('ryo', 5, 5, NOW(), NOW());
+
